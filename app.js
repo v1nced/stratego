dce58fbac7f8097738d0484b50a656d93cb8ecfb
FAXWON <you@example.com>
2023-04-28 21:43:35 +0600

    Тестовый Измение карты.

---
+++ app.js
@@ -0,0 +1,46 @@
+window.onload = function() {
+    var canvas = new fabric.Canvas('myCanvas');
+  
+    // Создаем объект карандаша
+    var pencilBrush = new fabric.PencilBrush(canvas);
+    pencilBrush.color = document.getElementById('pencil-color').value; // устанавливаем начальный цвет карандаша
+    pencilBrush.width = 5;
+  
+    // Создаем объект ластика
+    var eraserBrush = new fabric.CircleBrush(canvas);
+    eraserBrush.width = parseInt(document.getElementById('eraser-size').value, 10);
+    eraserBrush.globalCompositeOperation = 'destination-out';
+  
+    // Добавляем кнопку для выбора карандаша
+    document.getElementById('pencil-btn').addEventListener('click', function() {
+      canvas.freeDrawingBrush = pencilBrush;
+      canvas.isDrawingMode = true;
+    });
+  
+    // Добавляем кнопку для выбора ластика
+    document.getElementById('eraser-btn').addEventListener('click', function() {
+      canvas.freeDrawingBrush = eraserBrush;
+      canvas.isDrawingMode = true;
+    });
+  
+    // Добавляем возможность выбора цвета каранда
+  document.getElementById('pencil-color').addEventListener('change', function() {
+  pencilBrush.color = this.value;
+  });
+  
+  // Добавляем возможность изменения размера ластика
+  document.getElementById('eraser-size').addEventListener('change', function() {
+  eraserBrush.width = parseInt(this.value, 10);
+  });
+  
+  // Добавляем кнопку для очистки холста
+  document.getElementById('clear-btn').addEventListener('click', function() {
+  canvas.clear();
+  });
+  
+  // Добавляем возможность сохранения изображения
+  document.getElementById('save-btn').addEventListener('click', function() {
+  var dataURL = canvas.toDataURL();
+  window.open(dataURL);
+  });
+  };

2a315d602fe2453663fa1102478dd5c5506761c8
V1nced <josephsnow032@gmail.com>
2023-05-04 23:17:38 +0600

    Добавил Drag n Drop, но нужен рефактор (пока для первого канваса)

--- app.js
+++ app.js
@@ -174,7 +174,7 @@ const opIcon = document.querySelectorAll('#opicon')
 let isActive = [false,false,false];
 
 
-opIcon.forEach(element => {
+/* opIcon.forEach(element => {
 	element.addEventListener('click', () => {
 
 		
@@ -190,7 +190,7 @@ opIcon.forEach(element => {
 
 		
 	})
-})
+}) */
 
 
 function addOpIcon(canvas, element) {
@@ -309,3 +309,88 @@ removeDrawObj(canvas1)
 removeDrawObj(canvas2)
 
 canvas.getContext('2d', { willReadFrequently: true });
+
+
+function dragDrop(canvas){
+
+/* 
+NOTE: the start and end handlers are events for the <img> elements; the rest are bound to 
+the canvas container.
+*/
+
+function handleDragStart(e) {
+    [].forEach.call(images, function (img) {
+        img.classList.remove('img_dragging');
+    });
+    this.classList.add('img_dragging');
+}
+
+function handleDragOver(e) {
+    if (e.preventDefault) {
+        e.preventDefault(); // Necessary. Allows us to drop.
+    }
+
+    e.dataTransfer.dropEffect = 'copy'; // See the section on the DataTransfer object.
+    // NOTE: comment above refers to the article (see top) -natchiketa
+
+    return false;
+}
+
+function handleDragEnter(e) {
+    // this / e.target is the current hover target.
+    this.classList.add('over');
+}
+
+function handleDragLeave(e) {
+    this.classList.remove('over'); // this / e.target is previous target element.
+}
+
+function handleDrop(e) {
+    // this / e.target is current target element.
+
+    e.preventDefault(); //I've altert this line for FireFox
+
+    var img = document.querySelector('#images img.img_dragging');
+
+    console.log('event: ', e);
+
+    var newImage = new fabric.Image(img, {
+        width: img.width,
+        height: img.height,
+        // Set the center of the new object based on the event coordinates relative
+        // to the canvas container.
+        left: e.layerX,
+        top: e.layerY
+    });
+    canvas.add(newImage);
+
+    return false;
+}
+
+function handleDragEnd(e) {
+    // this/e.target is the source node.
+    [].forEach.call(images, function (img) {
+        img.classList.remove('img_dragging');
+    });
+}
+
+
+    // Browser supports HTML5 DnD.
+
+    // Bind the event listeners for the image elements
+    var images = document.querySelectorAll('#images img');
+    [].forEach.call(images, function (img) {
+        img.addEventListener('dragstart', handleDragStart, false);
+        img.addEventListener('dragend', handleDragEnd, false);
+    });
+    // Bind the event listeners for the canvas
+    var canvasContainer = document.querySelector('.canvas-container');
+    canvasContainer.addEventListener('dragenter', handleDragEnter, false);
+    canvasContainer.addEventListener('dragover', handleDragOver, false);
+    canvasContainer.addEventListener('dragleave', handleDragLeave, false);
+    canvasContainer.addEventListener('drop', handleDrop, false);
+
+ 
+}
+
+dragDrop(canvas)
